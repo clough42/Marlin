d1693b0bc75ba625c635df918db0c1d9b47d9c9d
clough42 <clough42@gmail.com>
2015-03-22 20:23:08 -0600

    Configure for double flex extruder.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -23,7 +23,7 @@
 // startup. Implementation of an idea by Prof Braino to inform user that any changes made to this
 // build by the user have been successfully uploaded into firmware.
 #define STRING_VERSION_CONFIG_H __DATE__ " " __TIME__ // build date and time
-#define STRING_CONFIG_H_AUTHOR "(clough42 double)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(clough42 dflex)" // Who made the changes.
 
 // SERIAL_PORT selects which serial port should be used for communication with the host.
 // This allows the connection of wireless adapters (for instance) to non-default port pins.
@@ -440,9 +440,9 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
 
 
   // these are the offsets to the probe relative to the extruder tip (Hotend - Probe)
-  #define X_PROBE_OFFSET_FROM_EXTRUDER 21
+  #define X_PROBE_OFFSET_FROM_EXTRUDER 34.5
   #define Y_PROBE_OFFSET_FROM_EXTRUDER -5 
-  #define Z_PROBE_OFFSET_FROM_EXTRUDER -2.29
+  #define Z_PROBE_OFFSET_FROM_EXTRUDER -2.00
 
   #define Z_RAISE_BEFORE_HOMING 4       // (in mm) Raise Z before homing (G28) for Probe Clearance.
                                         // Be sure you have this distance over your Z_MAX_POS in case
